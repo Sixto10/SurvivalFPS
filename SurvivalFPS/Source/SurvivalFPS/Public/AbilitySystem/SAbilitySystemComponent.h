3b63d1f8312bb26d25c081a2abd2f637ba4e5a23
Sixto <6togomez@gmail.com>
2023-05-21 22:30:51 -0400

    Started Implementing Gameplay Ability System
    Added 2 main classes to start creating the Gameplay Ability System.

---
+++ SurvivalFPS/Source/SurvivalFPS/Public/AbilitySystem/SAbilitySystemComponent.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "SAbilitySystemComponent.generated.h"
+
+/**
+ *  USAbilitySystemComponent
+ *
+ *	Base ability system component class used by this project.
+ */
+UCLASS()
+class SURVIVALFPS_API USAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
--- SurvivalFPS/Source/SurvivalFPS/SurvivalFPS.Build.cs
+++ SurvivalFPS/Source/SurvivalFPS/SurvivalFPS.Build.cs
@@ -9,5 +9,7 @@ public class SurvivalFPS : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+
+        PublicDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks" });
 	}
 }
